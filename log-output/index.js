75d8795d4cc6ca06c9d03e878db53034964da10e
Adam Farhadi <adam@adamfarhadi.com>
2025-12-01 16:21:48 +0100

    ex 1.7 done

--- log-output/index.js
+++ log-output/index.js
@@ -1,9 +1,23 @@
+const express = require('express')
+const app = express()
+const port = process.env.PORT
 const crypto = require("crypto");
 
+var currentStatus = ""
+
 const printString = () => {
-  console.log(new Date() + ': ' + crypto.randomUUID());
+  currentStatus = new Date() + ': ' + crypto.randomUUID()
+  console.log(currentStatus);
 
   setTimeout(printString, 5000)
 };
 
 printString()
+
+app.get('/status', (req, res) => {
+  res.send('<p>Current status: ' + currentStatus + '</p>')
+})
+
+app.listen(port, () => {
+  console.log(`Server started in port ${port}`)
+})
