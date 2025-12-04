bc8ab0260ec2a73c0b1aeee60d43970ea156cc79
Adam Farhadi <adam@adamfarhadi.com>
2025-12-04 09:37:20 +0100

    ex 1.13 working now with react + node setup, just need to add input

--- todo-app/todo-app-frontend/src/App.jsx
+++ todo-app/todo-app-frontend/src/App.jsx
@@ -3,9 +3,10 @@ function App() {
     <div>
       <h1>The Project App</h1>
       <img
-        src="http://localhost:3001/api/todo/image.jpg"
+        src="/api/todo/image.jpg"
         alt="random image from server"
       />
+      <p>DevOps with Kubernetes 2025</p>
     </div>
   )
 }
--- todo-app/todo-app-server/controllers/todo.js
+++ todo-app/todo-app-server/controllers/todo.js
@@ -1,6 +1,6 @@
 const todoRouter = require('express').Router()
 
-const image_file_path = '/tmp/image.jpg'
+const image_file_path = '/usr/src/app/files/image.jpg'
 
 todoRouter.get('/image.jpg', (req, res) => {
   res.sendFile(image_file_path)
