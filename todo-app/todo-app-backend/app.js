28b4239a0147649720c1dc7e1a7972321046e14f
Adam Farhadi <adam@adamfarhadi.com>
2025-12-05 17:19:38 +0100

    removing cors for ex 2.2 since it was unnecessary

--- todo-app/todo-app-backend/app.js
+++ todo-app/todo-app-backend/app.js
@@ -1,11 +1,9 @@
 const express = require('express')
 const todoRouter = require('./controllers/todos')
 const middleware = require('./utils/middleware')
-const cors = require('cors')
 
 const app = express()
 
-app.use(cors())
 app.use(express.json())
 
 app.use(middleware.requestLogger)
--- todo-app/todo-app/app.js
+++ todo-app/todo-app/app.js
@@ -1,18 +1,13 @@
 const express = require('express')
 const imageRouter = require('./controllers/image')
 const middleware = require('./utils/middleware')
-const cors = require('cors')
 
 const app = express()
-
-app.use(cors())
 app.use(express.json())
 
 if(process.env.NODE_ENV === 'production') {
   app.use(express.static('dist'))
 }
-
-// app.use(express.static('dist'))
 
 app.use(middleware.requestLogger)
 app.use('/image', imageRouter)
